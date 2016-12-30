1f0bd8a3dc6f1e091da0397a0985058b85a24216
e1r0nd <a.merezhanyi@gmail.com>
2016-12-30 20:53:52 +0100

    styling

--- Jaden Casing Strings (7 kyu).js
+++ Jaden Casing Strings (7 kyu).js
@@ -1,4 +1,5 @@
 /** Jaden Casing Strings (7 kyu)
+ *
  * Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013).
  * Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter
  * he is known for almost always capitalizing every word.
