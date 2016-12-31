d929e66663c65cf3ff0b18020aa5d36377723426
e1r0nd <a.merezhanyi@gmail.com>
2016-12-31 21:36:33 +0100

    add url

--- Jaden Casing Strings (7 kyu).js
+++ Jaden Casing Strings (7 kyu).js
@@ -1,4 +1,4 @@
-/** Jaden Casing Strings (7 kyu)
+/** Jaden Casing Strings (7 kyu) https://www.codewars.com/kata/jaden-casing-strings/train/javascript
  *
  * Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013).
  * Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter
