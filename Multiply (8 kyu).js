43b34b1f907967dfd93e91b7b682e17e22103a00
e1r0nd <a.merezhanyi@gmail.com>
2016-12-31 21:35:47 +0100

    ulr added

--- Multiply (8 kyu).js
+++ Multiply (8 kyu).js
@@ -1,4 +1,4 @@
-/** Multiply (8 kyu)
+/** Multiply (8 kyu) https://www.codewars.com/kata/multiply/train/javascript
  * The code does not execute properly. Try to figure out why.
 **/
 function multiply(a, b){
