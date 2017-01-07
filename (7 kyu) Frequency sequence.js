e904e616a0ed89900547387cce402478747f1283
e1r0nd <a.merezhanyi@gmail.com>
2017-01-07 18:31:34 +0100

    rename it

---
+++ (7 kyu) Frequency sequence.js
@@ -0,0 +1,25 @@
+/** Frequency sequence (7 kyu) https://www.codewars.com/kata/frequency-sequence/train/javascript
+ * Return an output string that translates an input string s/$s by
+ * replacing each character in s/$s with a number representing the number
+ * of times that character occurs in s/$s and separating each number with
+ * the character(s) sep/$sep.
+ *
+ * Example:
+ * freqSeq('hello world', '-'); // => '1-1-3-3-2-1-1-2-1-3-1'
+ * freqSeq('^^^**$',      'x'); // => '3x3x3x2x2x1'
+ * freqSeq('19999999',    ':'); // => '1:7:7:7:7:7:7:7'
+**/
+function freqSeq(str, sep) {
+  var freq = {};
+  var newStr = '';
+  
+  for (let i = 0; i < str.length; i++) {
+    freq[str[i]] = (freq[str[i]]) ? freq[str[i]] += 1 : 1;
+  }
+  
+  for (let i = 0; i < str.length; i++) {
+    newStr += (i) ? sep + freq[str[i]] : freq[str[i]];
+  }
+  
+  return newStr;
+}
