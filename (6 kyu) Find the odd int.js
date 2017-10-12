35ef527b48b9fc7a3b8a9f11aa5dcf693a80e899
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-10-12 09:36:58 +0200

    Update (6 kyu) Find the odd int.js

--- (6 kyu) Find the odd int.js
+++ (6 kyu) Find the odd int.js
@@ -1,4 +1,4 @@
-/** Find the odd int (6 kyu).js https://www.codewars.com/kata/find-the-odd-int/train/javascript
+/** Find the odd int (6 kyu).js https://www.codewars.com/kata/find-the-odd-int/
  * Given an array, find the int that appears an odd number of times.
  * There will always be only one integer that appears an odd number of times.
 **/
