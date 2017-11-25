33996b2fbd54398afbaf5d3bdfece7ce8660fe22
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-11-26 00:06:23 +0100

    added Strip Comments

---
+++ (4 kyu) Strip Comments.js
@@ -0,0 +1,23 @@
+/**
+ * Strip Comments (4 kyu) https://www.codewars.com/kata/51c8e37cee245da6b40000bd
+ * Complete the solution so that it strips all text that follows any of a
+ * set of comment markers passed in. Any whitespace at the end of the line
+ * should also be stripped out.
+ * 
+ * Example:
+ * Given an input string of:
+ * apples, pears # and bananas
+ * grapes
+ * bananas !apples
+ * The output expected would be:
+ * apples, pears
+ * grapes
+ * bananas
+ */
+const solution = (input, markers) =>
+  input
+    .split('\n')
+    .map(s => markers
+        .reduce((t, m) => t.split(m)[0].trim(), s)
+    )
+    .join('\n');
