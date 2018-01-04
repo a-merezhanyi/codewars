7c1f92c14cb2e0c4852a73b0448de5a4b45388ce
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-01-04 10:04:56 +0100

    added description to 9999

--- (5 kyu) Ninety Nine Thousand Nine Hundred Ninety Nine.js
+++ (5 kyu) Ninety Nine Thousand Nine Hundred Ninety Nine.js
@@ -1,3 +1,13 @@
+/**
+ * Ninety Nine Thousand Nine Hundred Ninety Nine (5 kyu)
+ * https://www.codewars.com/kata/ninety-nine-thousand-nine-hundred-ninety-nine
+ * Write a method that takes a number and returns a string of that number in
+ * English. For example
+ * numberToEnglish(27) // => 'twenty seven'
+ * Your method should be able to handle any number between 0 and 99999. If
+ * given numbers outside of that range or non-Integer numbers, the method
+ * should return an empty string.
+ */
 const numberToEnglish = x => {
   let n = x.toString();
   
