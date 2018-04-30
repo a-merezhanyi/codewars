b37664125dd8cf4bef1f3cdc266ba7ac5a1972c4
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-30 08:44:30 +0200

    solved: Abbreviate a Two Word Name (8 kyu)

---
+++ (8 kyu) Abbreviate a Two Word Name.js
@@ -0,0 +1,13 @@
+/** Abbreviate a Two Word Name (8 kyu)
+ * https://www.codewars.com/kata/abbreviate-a-two-word-name
+ *
+ * Write a function to convert a name into initials. This kata strictly takes
+ * two words with one space in between them.
+ * The output should be two capital letters with a dot seperating them.
+ * It should look like this:
+ * Sam Harris => S.H
+ * Patrick Feeney => P.F
+ */
+function abbrevName(name) {
+  return name.split` `.map(s => s[0].toUpperCase()).join`.`;
+}
