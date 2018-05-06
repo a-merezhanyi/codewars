a5626e5fbfc238cb2f1085d833e91e7afe3e5fc7
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-05-06 17:40:20 +0200

    Update (8 kyu) Find the smallest integer in the array.js

--- (8 kyu) Find the smallest integer in the array.js
+++ (8 kyu) Find the smallest integer in the array.js
@@ -1,4 +1,5 @@
 /** (8 kyu) Find the smallest integer in the array
+ * https://www.codewars.com/kata/find-the-smallest-integer-in-the-array
  * Given an array of integers your solution should find the smallest integer.
  * For example:
  * Given [34, 15, 88, 2] your solution will return 2
