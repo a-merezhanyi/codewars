226267be3e1a713d9f7b8e0a211404678577cafb
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-05-27 14:37:45 +0200

    Fixed .py file

--- (8 kyu) Multiply.py
+++ (8 kyu) Multiply.py
@@ -1,6 +1,6 @@
-/** Multiply (8 kyu) https://www.codewars.com/kata/multiply/train/javascript
- * The code does not execute properly. Try to figure out why.
-**/
+# Multiply (8 kyu) https://www.codewars.com/kata/multiply/train/javascript
+# The code does not execute properly. Try to figure out why.
+
 function multiply(a, b){
   return a * b
 }
