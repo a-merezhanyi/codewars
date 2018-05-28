ee56cb057cc802fe796e9f2c0eec7dc5d5f962b4
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-05-28 12:40:49 +0200

    refactoring

--- (7 kyu) Canvas Fun 1 - Draw Lines.js
+++ (7 kyu) Canvas Fun 1 - Draw Lines.js
@@ -18,15 +18,27 @@ function drawLines(points) {
   ctx.beginPath();
   //Don't delete or modify the code above
   //Your code starts here:
-
+  /* #1
   const xCoordinate = 0;
   const yCoordinate = 1;
-  // start from 0 point
   ctx.moveTo(points[0][xCoordinate], points[0][yCoordinate]);
   for (let i = 1; i < points.length; i++) {
-    // draw a line to the next point
     ctx.lineTo(points[i][xCoordinate], points[i][yCoordinate]);
   }
+  */
+  /* #2
+  ctx.moveTo(...points[0])
+  for (let i = 1; i < points.length; i++) {
+    ctx.lineTo(...points[i]);
+  }
+  */
+  /* #3 */
+  // start from 0 point
+  ctx.moveTo(...points.shift());
+  points.forEach(p =>
+    // draw a line to the next point
+    ctx.lineTo(...p)
+  );
 
   //Don't delete or modify the following code
   ctx.stroke(); //Draw the path you made above
