854e2ec40b6bcebfe7a0c6f08a252bbbbca111c0
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-21 18:55:15 +0200

    Update (8 kyu) Multiply.rs

--- (8 kyu) Multiply.rs
+++ (8 kyu) Multiply.rs
@@ -1,6 +1,6 @@
-/** Multiply (8 kyu) https://www.codewars.com/kata/multiply/train/javascript
+/** Multiply (8 kyu) https://www.codewars.com/kata/multiply/train/rust
  * The code does not execute properly. Try to figure out why.
 **/
 fn multiply(a:i32, b:i32) -> i32 {
-  return a * b
+  a * b
 }
