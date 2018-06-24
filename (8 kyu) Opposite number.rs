8045f4f66c511839a7eef5497306368249bf4d41
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-25 00:28:13 +0200

    solved "Opposite number" in Rust

---
+++ (8 kyu) Opposite number.rs
@@ -0,0 +1,11 @@
+/// Opposite number (8 kyu)
+/// https://www.codewars.com/kata/opposite-number
+/// Very simple, given a number, find its opposite.
+/// But can you do it in 1 line of code and without any conditionals?
+fn opposite(number: i32) -> i32 {
+    // #1
+    // let result: i32 = number * -1;
+    // return result;
+    // #2
+    -number
+}
