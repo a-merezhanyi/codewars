e427dce253969cb00a2ce99852a5ec08dc1d3d4a
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-02 22:29:49 +0200

    rust: Even or Odd (8 kyu)

---
+++ (8 kyu) Even or Odd.rs
@@ -0,0 +1,14 @@
+/// Even or Odd (8 kyu)
+/// https://www.codewars.com/kata/even-or-odd
+/// Create a function that takes an integer as an argument and returns
+/// "Even" for even numbers or "Odd" for odd numbers.
+fn even_or_odd(i: i32) -> &'static str {
+    // #1
+    // if i % 2 == 0 {
+    //     return "Even";
+    // } else {
+    //     return "Odd";
+    // }
+    // #2
+    return if i % 2 == 0 { "Even" } else { "Odd" };
+}
