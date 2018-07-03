e13f9f4967e661f71cf8b31c3106be4a471ca1b7
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-03 10:22:22 +0200

    rust: Reversed Strings

---
+++ (8 kyu) Reversed Strings.rs
@@ -0,0 +1,8 @@
+/// Reversed Strings (8 kyu)
+/// https://www.codewars.com/kata/reversed-strings
+/// Complete the solution so that it reverses the string value passed
+/// into it.
+/// solution("world") // returns "dlrow"
+fn solution(phrase: &str) -> String {
+    phrase.chars().rev().collect::<String>()
+}
