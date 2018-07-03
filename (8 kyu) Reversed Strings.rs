c6df91bf8806a05a691c90362bb9dc42b56a6432
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-03 21:38:34 +0200

    refactoring

--- (8 kyu) Reversed Strings.rs
+++ (8 kyu) Reversed Strings.rs
@@ -4,5 +4,8 @@
 /// into it.
 /// solution("world") // returns "dlrow"
 fn solution(phrase: &str) -> String {
-    phrase.chars().rev().collect::<String>()
+    // #1
+    // phrase.chars().rev().collect::<String>()
+    // #2
+    phrase.chars().rev().collect()
 }
