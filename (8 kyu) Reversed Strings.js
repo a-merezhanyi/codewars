266d5557390eddafca799f4698a15328008c2dcb
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-03 21:43:12 +0200

    js: Reversed Strings

---
+++ (8 kyu) Reversed Strings.js
@@ -0,0 +1,13 @@
+/** Reversed Strings (8 kyu)
+ * https://www.codewars.com/kata/reversed-strings
+ *
+ * Complete the solution so that it reverses the string value passed
+ * into it.
+ * solution('world'); // returns 'dlrow'
+ */
+// #1
+// function solution(str){
+//     return str.split("").reverse().join("");
+// }
+// #2
+const solution = (s) => s.split``.reverse().join``;
