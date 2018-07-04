e227ceae0fede0713ad0046a2eece31d6149d8fa
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-04 21:34:34 +0200

    js refactored

--- (8 kyu) The Feast of Many Beasts.js
+++ (8 kyu) The Feast of Many Beasts.js
@@ -1,29 +1,35 @@
 /** The Feast of Many Beasts (8 kyu)
  * https://www.codewars.com/kata/the-feast-of-many-beasts/
- * All of the animals are having a feast! Each animal is bringing one dish.
- * There is just one rule: the dish must start and end with the same letters as
- * the animal's name. For example, the great blue heron is bringing garlic naan
- * and the chickadee is bringing chocolate cake.
+ * All of the animals are having a feast! Each animal is bringing one
+ * dish. There is just one rule: the dish must start and end with the
+ * same letters as the animal's name. For example, the great blue heron
+ * is bringing garlic naan and the chickadee is bringing chocolate cake.
  *
- * Write a function feast that takes the animal's name and dish as arguments
- * and returns true or false to indicate whether the beast is allowed to bring
- * the dish to the feast.
+ * Write a function feast that takes the animal's name and dish as
+ * arguments and returns true or false to indicate whether the beast is
+ * allowed to bring the dish to the feast.
  *
- * Assume that beast and dish are always lowercase strings, and that each has
- * at least two letters. beast and dish may contain hyphens and spaces, but
- * these will not appear at the beginning or end of the string. They will not
- * contain numerals.
+ * Assume that beast and dish are always lowercase strings, and that each
+ * has at least two letters. beast and dish may contain hyphens and
+ * spaces, but these will not appear at the beginning or end of the
+ * string. They will not contain numerals.
  **/
-function feast_1st(beast, dish) {
-  let result
-  const start = beast[0] === dish[0]
-  // const end = beast[beast.length - 1] === dish[dish.length - 1]
-  const end = beast.slice(-1) === dish.slice(-1);
-  result = start && end
-  
-  return result
-}
-
+// #1
+// function feast(beast, dish) {
+//   let result;
+//   const start = beast[0] === dish[0];
+//   const end = beast[beast.length - 1] === dish[dish.length - 1];
+//   result = start && end;
+//   return result;
+// }
+// #2
+// function feast(beast, dish) {
+//   let result;
+//   const start = beast[0] === dish[0];
+//   const end = beast.slice(-1) === dish.slice(-1);
+//   result = start && end;
+//   return result;
+// }
+// #3
 const feast = (beast, dish) =>
-  beast[0] === dish[0]
-  && beast.slice(-1) === dish.slice(-1);
+  beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1);
