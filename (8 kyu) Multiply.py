9598bb69e8db2d2ca86f9571e311bed4febf70bd
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-04 21:45:47 +0200

    py: refactored

--- (8 kyu) Multiply.py
+++ (8 kyu) Multiply.py
@@ -1,6 +1,6 @@
-# Multiply (8 kyu) https://www.codewars.com/kata/multiply/train/javascript
-# The code does not execute properly. Try to figure out why.
-
-function multiply(a, b){
+""" Multiply (8 kyu)
+https://www.codewars.com/kata/multiply/train/javascript
+The code does not execute properly. Try to figure out why.
+"""
+def multiply(a, b):
   return a * b
-}
--- (8 kyu) Reversed sequence.py
+++ (8 kyu) Reversed sequence.py
@@ -1,8 +1,9 @@
-# Reversed sequence (8 kyu)
-# https://www.codewars.com/kata/reversed-sequence
-#
-# Get the number n (n>0) to return the reversed sequence from n to 1.
-# Example : n=5 >> [5,4,3,2,1]
+""" Reversed sequence (8 kyu)
+https://www.codewars.com/kata/reversed-sequence
+
+Get the number n (n>0) to return the reversed sequence from n to 1.
+Example : n=5 >> [5,4,3,2,1]
+"""
 
 
 def reverse_seq(n):
