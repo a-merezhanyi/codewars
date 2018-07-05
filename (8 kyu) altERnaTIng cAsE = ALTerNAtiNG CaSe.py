0eb9354110e1646a511eb05ceaaedad478639f2b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-05 21:22:31 +0200

    py: solved altERnaTIng cAsE

---
+++ (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.py
@@ -0,0 +1,28 @@
+"""
+altERnaTIng cAsE <=> ALTerNAtiNG CaSe (8 kyu)
+http://www.codewars.com/kata/alternating-case-%3C-equals-%3E-alternating-case
+Define String.prototype.toAlternatingCase (or a similar
+function/method such as
+to_alternating_case/toAlternatingCase/ToAlternatingCase in your
+selected language; see the initial solution for details) such that
+each lowercase letter becomes uppercase and each uppercase letter
+becomes lowercase. For example:
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"""
+
+
+def to_alternating_case(string):
+    # #1
+    # result = []
+    # for x in string:
+    #     if x.islower():
+    #         result.append(x.upper())
+    #     else:
+    #         result.append(x.lower())
+    # return ''.join(result)
+    # #2
+    # return ''.join(list(map(lambda s:s.upper() if s.islower() else s.lower(),string)))
+    # #3
+    # return ''.join([c.upper() if c.islower() else c.lower() for c in string])
+    # #4
+    return string.swapcase()
