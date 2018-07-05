711312a89a3464a9163c5b76e4fbaea813bd3dfc
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-05 21:31:03 +0200

    clean up

--- (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.py
+++ (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.py
@@ -11,18 +11,23 @@ becomes lowercase. For example:
 """
 
 
-def to_alternating_case(string):
-    # #1
-    # result = []
-    # for x in string:
-    #     if x.islower():
-    #         result.append(x.upper())
-    #     else:
-    #         result.append(x.lower())
-    # return ''.join(result)
-    # #2
-    # return ''.join(list(map(lambda s:s.upper() if s.islower() else s.lower(),string)))
-    # #3
-    # return ''.join([c.upper() if c.islower() else c.lower() for c in string])
-    # #4
-    return string.swapcase()
+# # #1
+# def to_alternating_case(string):
+#     result = []
+#     for x in string:
+#         if x.islower():
+#             result.append(x.upper())
+#         else:
+#             result.append(x.lower())
+#     return ''.join(result)
+# # #2
+# def to_alternating_case(string):
+#     return ''.join(list(map(lambda s:s.upper() if s.islower() else s.lower(),string)))
+# # 3
+# def to_alternating_case(string):
+#     return ''.join([c.upper() if c.islower() else c.lower() for c in string])
+# #4
+# def to_alternating_case(string):
+#     return string.swapcase()
+# #5
+to_alternating_case = str.swapcase
