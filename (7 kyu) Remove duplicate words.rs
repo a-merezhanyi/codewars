abf4140a21fcfdf3dc835bd5a80336fcb6f9b2e8
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-13 23:18:47 +0200

    restore

--- (7 kyu) Remove duplicate words.rs
+++ (7 kyu) Remove duplicate words.rs
@@ -8,33 +8,24 @@
 use std::collections::HashSet;
 
 fn remove_duplicate_words(s: &str) -> String {
-    // #1
-    // let v: Vec<&str> = s.split(" ").collect();
-    // let mut set = HashSet::new();
-    // let mut arr = Vec::new();
-    // let mut res = String::new();
+    let v: Vec<&str> = s.split(" ").collect();
+    let mut set = HashSet::new();
+    let mut arr = Vec::new();
+    let mut res = String::new();
 
-    // for i in 0..v.len() {
-    //     if !set.contains(&v[i]) {
-    //         set.insert(&v[i]);
-    //         arr.push(&v[i]);
-    //     }
-    // }
-    // for i in 0..arr.len() {
-    //     if i == arr.len() - 1 {
-    //         res.push_str(arr[i]);
-    //     } else {
-    //         res.push_str(&String::from(format!("{} ", arr[i])));
-    //     }
-    // }
-
-    // res
-    // #2
-    let r = s.split(" ").fold(String::from(""), |mut r, a| {
-        if !r.contains(a) {
-            r.push_str(&format!("{} ", a));
+    for i in 0..v.len() {
+        if !set.contains(&v[i]) {
+            set.insert(&v[i]);
+            arr.push(&v[i]);
         }
-        r
-    });
-    r.trim().to_string()
+    }
+    for i in 0..arr.len() {
+        if i == arr.len() - 1 {
+            res.push_str(arr[i]);
+        } else {
+            res.push_str(&String::from(format!("{} ", arr[i])));
+        }
+    }
+
+    res
 }
