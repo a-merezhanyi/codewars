a959bb539cbcb47d5f89929c8438c5087466506a
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-14 09:17:27 +0200

    py: Opposite number

---
+++ (8 kyu) Opposite number.py
@@ -0,0 +1,12 @@
+""" Opposite number (8 kyu)
+https://www.codewars.com/kata/opposite-number
+Very simple, given a number, find its opposite.
+But can you do it in 1 line of code and without any conditionals?
+"""
+# #1
+# def opposite(number):
+#   return -number
+# #2
+
+
+def opposite(number): return -number
