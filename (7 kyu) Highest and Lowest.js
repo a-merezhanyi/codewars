0a79ba73a3148d5a2fcf2dd5f5f8cd0a755a0f6f
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-15 21:06:06 +0200

    py: Highest and Lowest

--- (7 kyu) Highest and Lowest.js
+++ (7 kyu) Highest and Lowest.js
@@ -1,16 +1,18 @@
-/** Highest and Lowest (7 kyu) https://www.codewars.com/kata/highest-and-lowest
- * In this little assignment you are given a string of space separated numbers,
- * and have to return the highest and lowest number.
+/** Highest and Lowest (7 kyu)
+ * https://www.codewars.com/kata/highest-and-lowest
+ * In this little assignment you are given a string of space separated
+ * numbers, and have to return the highest and lowest number.
  *
  * Example:
  * highAndLow("1 2 3 4 5"); // return "5 1"
  * highAndLow("1 2 -3 4 5"); // return "5 -3"
  * highAndLow("1 9 3 4 -5"); // return "9 -5"
  *
- * NOTE: 
+ * NOTE:
  * All numbers are valid Int32, no need to validate them.
  * There will always be at least one number in the input string.
- * Output string must be two numbers separated by a single space, and highest
- * number is first.
-**/
-const highAndLow = (numbers) => `${Math.max(...numbers.split(' '))} ${Math.min(...numbers.split(' '))}`
+ * Output string must be two numbers separated by a single space, and
+ * highest number is first.
+ **/
+const highAndLow = (numbers) =>
+  `${Math.max(...numbers.split(' '))} ${Math.min(...numbers.split(' '))}`;
