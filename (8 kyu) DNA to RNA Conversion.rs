ef4f95bce1834e4b64fbcbd47d26e8ec333d2575
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-20 22:10:45 +0200

    rs: DNA to RNA Conversion (8 kyu)

---
+++ (8 kyu) DNA to RNA Conversion.rs
@@ -0,0 +1,24 @@
+/// DNA to RNA Conversion (8 kyu)
+/// https://www.codewars.com/kata/dna-to-rna-conversion
+/// Deoxyribonucleic acid, DNA is the primary information storage molecule
+/// in biological systems. It is composed of four nucleic acid bases
+/// Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+/// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA
+/// differs slightly from DNA its chemical structure and contains no
+/// Thymine. In RNA Thymine is replaced by another nucleic acid
+/// Uracil ('U').
+/// Create a funciton which translates a given DNA string into RNA.
+/// For example:
+/// DNAtoRNA("GCAT") returns ("GCAU")
+
+fn dna_to_rna(dna: &str) -> String {
+    // #1
+    // dna.chars()
+    //     .map(|x| match x {
+    //         'T' => 'U',
+    //         _ => x,
+    //     })
+    //     .collect()
+    // #2
+    str::replace(dna, "T", "U")
+}
