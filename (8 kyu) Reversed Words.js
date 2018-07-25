6f02be5a70c43d823984c52fc0bf16c9c4d249d6
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-25 13:32:25 +0200

    js: Reversed Words

---
+++ (8 kyu) Reversed Words.js
@@ -0,0 +1,15 @@
+/** Reversed Words (8 kyu)
+ * https://www.codewars.com/kata/reversed-words
+ * Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+*/
+// #1
+// function reverseWords(str) {
+//   return str.split` `.reverse().join` `;
+// }
+// #2
+const reverseWords = (str) => str.split` `.reverse().join` `;
