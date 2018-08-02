731c59c4b0aae639ef57764d2425ed3a397fd509
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-08-02 22:17:50 +0200

    rs: (7 kyu) Highest and Lowest

---
+++ (7 kyu) Highest and Lowest/(7 kyu) Highest and Lowest.rs
@@ -0,0 +1,25 @@
+// #1
+// fn high_and_low(numbers: &str) -> String {
+//     let mut res = String::new();
+//     let arr_str = numbers.split_whitespace().collect::<Vec<_>>();
+//     let arr_num = arr_str
+//         .iter()
+//         .map(|&s| s.parse::<i32>().unwrap())
+//         .collect::<Vec<i32>>();
+
+//     res.push_str(&arr_num.iter().max().unwrap().to_string());
+//     res.push_str(&" ".to_string());
+//     res.push_str(&arr_num.iter().min().unwrap().to_string());
+//     res
+// }
+// #2
+fn high_and_low(numbers: &str) -> String {
+    let arr_str = String::from(numbers);
+    let arr_num: Vec<i32> = arr_str
+        .split_whitespace()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+    let low = arr_num.iter().min().unwrap();
+    let high = arr_num.iter().max().unwrap();
+    format!("{} {}", high, low)
+}
