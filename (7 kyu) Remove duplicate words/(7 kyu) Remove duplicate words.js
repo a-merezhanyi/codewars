46369b379c08b14c84f192b3d32a23c2c09e954f
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-09-03 19:27:22 +0200

    Update (7 kyu) Remove duplicate words.js

--- (7 kyu) Remove duplicate words/(7 kyu) Remove duplicate words.js
+++ (7 kyu) Remove duplicate words/(7 kyu) Remove duplicate words.js
@@ -1,9 +1,1 @@
-/** Remove duplicate words (7 kyu)
- * http://www.codewars.com/kata/remove-duplicate-words
- * Your task is to remove all duplicate words from string, leaving only
- * single words entries.
- * Input: 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma
- * gamma gamma delta'
- * Output: 'alpha beta gamma delta'
- */
 const removeDuplicateWords = (s) => [...new Set(s.split` `)].join` `;
