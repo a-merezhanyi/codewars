509ed6cc6bd715c730989c70121249e21fdc5d4d
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-09-03 19:26:07 +0200

    Update (7 kyu) Remove duplicate words.py

--- (7 kyu) Remove duplicate words/(7 kyu) Remove duplicate words.py
+++ (7 kyu) Remove duplicate words/(7 kyu) Remove duplicate words.py
@@ -1,11 +1,3 @@
-""" Remove duplicate words (7 kyu)
-http://www.codewars.com/kata/remove-duplicate-words
-Your task is to remove all duplicate words from string, leaving only
-single words entries.
-Input: 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma
-gamma gamma delta'
-Output: 'alpha beta gamma delta'
-"""
 # #1 py 3.4.3
 # from collections import OrderedDict
 # def remove_duplicate_words(s):
