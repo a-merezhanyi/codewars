2b13d4d8f4f43066cbb5b3367b9f124b2c879b61
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-09-04 22:24:06 +0200

    Update (7 kyu) Highest and Lowest.py

--- (7 kyu) Highest and Lowest/(7 kyu) Highest and Lowest.py
+++ (7 kyu) Highest and Lowest/(7 kyu) Highest and Lowest.py
@@ -1,19 +1,3 @@
-""" Highest and Lowest (7 kyu)
-https://www.codewars.com/kata/highest-and-lowest
-In this little assignment you are given a string of space separated
-numbers, and have to return the highest and lowest number.
-
-Example:
-highAndLow("1 2 3 4 5"); // return "5 1"
-highAndLow("1 2 -3 4 5"); // return "5 -3"
-highAndLow("1 9 3 4 -5"); // return "9 -5"
-
-NOTE: 
-All numbers are valid Int32, no need to validate them.
-There will always be at least one number in the input string.
-Output string must be two numbers separated by a single space, and highest
-number is first.
-"""
 def high_and_low(numbers):
     arr = list(map(int, numbers.split()))
     return str(max(arr)) + ' ' + str(min(arr))
