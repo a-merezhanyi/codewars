572d69b562c4e69bd815fef99d3357d0ea2887bb
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-11-14 12:47:40 +0100

    refactoring abbrevName

--- (8 kyu) Abbreviate a Two Word Name/(8 kyu) Abbreviate a Two Word Name.js
+++ (8 kyu) Abbreviate a Two Word Name/(8 kyu) Abbreviate a Two Word Name.js
@@ -1,3 +1,31 @@
-function abbrevName(name) {
-  return name.split` `.map((s) => s[0].toUpperCase()).join`.`;
-}
+// #1
+// function abbrevName(name) {
+//   const arr = name.split(' ');
+//   const firstname = arr[0];
+//   const surname = arr[1];
+//   const result = firstname[0].toUpperCase() + '.' + surname[0].toUpperCase();
+//   return result;
+// }
+// #2
+// function abbrevName(name){
+//   const arr = name.split(' ')
+//   const [firstname, surname] = arr
+//   const result = `${firstname[0].toUpperCase()}.${surname[0].toUpperCase()}`
+//   return result
+// }
+// #3
+// function abbrevName(name){
+//   const arr = name.split(' ')
+//   const result = arr.map(s => s[0].toUpperCase()).join('.')
+//   return result
+// }
+// #4
+// function abbrevName(name) {
+//   return name.split` `.map((s) => s[0].toUpperCase()).join`.`;
+// }
+// #5
+const abbrevName = (name) =>
+  name
+    .split(' ')
+    .map((s) => s[0].toUpperCase())
+    .join('.');
