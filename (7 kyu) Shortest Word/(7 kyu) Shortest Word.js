a166f6a101306b42c8c90d5d6d2f9ae67ce99940
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-12 00:37:00 +0100

    clean up

--- (7 kyu) Shortest Word/(7 kyu) Shortest Word.js
+++ (7 kyu) Shortest Word/(7 kyu) Shortest Word.js
@@ -1,1 +1,19 @@
+// #1
+// function findShort(s) {
+//   const arr = s.split(' ');
+//   let smallest = arr[0];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length < smallest.length) {
+//       smallest = arr[i];
+//     }
+//   }
+//   return smallest.length;
+// }
+// #2
+// const findShort = (s) =>
+//   s
+//     .split(' ')
+//     .sort((a, b) => b.length - a.length)
+//     .pop().length;
+// #3
 const findShort = (s) => Math.min(...s.split` `.map((w) => w.length));
