3341e2055453c6b60d0465492e5aaa6f24cf7427
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-12 01:25:51 +0100

    refactoring

--- (7 kyu) Remove duplicate words/(7 kyu) Remove duplicate words.js
+++ (7 kyu) Remove duplicate words/(7 kyu) Remove duplicate words.js
@@ -1,1 +1,19 @@
+// #1
+// function removeDuplicateWords(s) {
+//   let cleanArr = [];
+//   s.split(' ').forEach((current) => {
+//     if (!cleanArr.includes(current)) {
+//       cleanArr.push(current);
+//     }
+//   });
+//   return cleanArr.join(' ');
+// }
+// #2
+// function removeDuplicateWords(s) {
+//   return s
+//     .split(' ')
+//     .filter((el, index, arr) => arr.indexOf(el) == index)
+//     .join(' ');
+// }
+// #3
 const removeDuplicateWords = (s) => [...new Set(s.split` `)].join` `;
