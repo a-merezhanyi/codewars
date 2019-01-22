211736209361f7620a9ed3b788fca37720abdd40
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-22 13:45:55 +0100

    refactoring

--- (8 kyu) Abbreviate a Two Word Name/(8 kyu) Abbreviate a Two Word Name.cpp
+++ (8 kyu) Abbreviate a Two Word Name/(8 kyu) Abbreviate a Two Word Name.cpp
@@ -5,28 +5,35 @@ std::string abbrevName(std::string name)
 {
     // #1
     // size_t pos = name.find(" ");
-    // char first_name = std::toupper(name[0]);
-    // char last_name = std::toupper(name[pos + 1]);
+    // char first_initial = std::toupper(name[0]);
+    // char second_initial = std::toupper(name[pos + 1]);
 
-    // std::string abr;
-    // abr += first_name;
-    // abr += ".";
-    // abr += last_name;
+    // std::string abbr;
+    // abbr += first_initial;
+    // abbr += ".";
+    // abbr += second_initial;
 
-    // return abr;
+    // return abbr;
 
     // #2
-    // std::string ret;
-    // ret.push_back(toupper(name[0]));
-    // ret.push_back('.');
-    // ret.push_back(toupper(name[name.find(' ') + 1]));
-    // return ret;
+    // std::string abbr;
+    // abbr.push_back(toupper(name[0]));
+    // abbr.push_back('.');
+    // abbr.push_back(toupper(name[name.find(' ') + 1]));
+    // return abbr;
 
     // #3
+    // std::string abbr = "";
+    // abbr += toupper(name[0]);
+    // abbr += '.';
+    // abbr += toupper(name[name.find(' ') + 1]);
+    // return abbr;
+
+    // #4
     // std::string first_initial{toupper(name[0])};
     // std::string second_initial{toupper(name[name.find_last_of(' ') + 1])};
     // return first_initial + "." + second_initial;
 
-    // #4
+    // #5
     return {toupper(name[0]), '.', toupper(name[name.find_last_of(' ') + 1])};
 }
