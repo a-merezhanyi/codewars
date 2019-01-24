eac3e431f1239417d0855bc1e0607c6a7a029b4d
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-24 19:50:38 +0100

    (6 kyu) IQ Test - refactored

--- (6 kyu) IQ Test/(6 kyu) IQ Test.js
+++ (6 kyu) IQ Test/(6 kyu) IQ Test.js
@@ -1,13 +1,32 @@
+// #1 Using for loop
+// function iqTest(numbers) {
+//   numbers = numbers.split(' ');
+//   const evens = [];
+//   const odds = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     numbers[i] & 1 ? odds.push(i + 1) : evens.push(i + 1);
+//   }
+//   return evens.length === 1 ? evens[0] : odds[0];
+// }
+// #2 Using "some"
+// const iqTest = (numbers) => {
+//   const evenOdd = [[0, 0], [0, 0]];
+//   numbers.split(' ').some((num, i) => {
+//     const n = num % 2;
+//     evenOdd[n][0]++; // store quantity
+//     evenOdd[n][1] = i; // store its index
+//     if (evenOdd[n][0] > 1 && evenOdd[+!n][0] == 1) {
+//       return true; // stop iterating when found
+//     }
+//   });
+//   return ++evenOdd[evenOdd[0][0] == 1 ? 0 : 1][1];
+// };
+// #3 Using "filter"
 const iqTest = (numbers) => {
-  let evenOdd = [[0, 0], [0, 0]];
-
-  numbers.split(' ').some(function(num, i) {
-    let n = num % 2;
-    evenOdd[n][0]++; // store quantity
-    evenOdd[n][1] = i; // store its index
-    if (evenOdd[n][0] > 1 && evenOdd[+!n][0] == 1) {
-      return true; // stop iterating when found
-    }
-  });
-  return ++evenOdd[evenOdd[0][0] == 1 ? 0 : 1][1];
+  numbers = numbers.split(' ').map((el) => +el);
+  var odd = numbers.filter((el) => el % 2 === 1);
+  var even = numbers.filter((el) => el % 2 === 0);
+  return odd.length < even.length
+    ? numbers.indexOf(odd[0]) + 1
+    : numbers.indexOf(even[0]) + 1;
 };
