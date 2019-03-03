b0015866a33fc800ad13fc4decde75a7f1809e7b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-03-03 23:39:30 +0100

    refactoring

--- (7 kyu) Mumbling/(7 kyu) Mumbling.cpp
+++ (7 kyu) Mumbling/(7 kyu) Mumbling.cpp
@@ -1,24 +1,42 @@
 #include <string>
 #include <cctype>
 
+// #1
+// class Accumul
+// {
+//   public:
+//     static std::string accum(const std::string &s)
+//     {
+//         std::string res;
+//         for (int i = 0; i < s.length(); i++)
+//         {
+//             std::string word;
+//             for (int j = 0; j <= i; j++)
+//                 if (j == 0)
+//                     word += std::toupper(s[i]);
+//                 else
+//                     word += std::tolower(s[i]);
+//             res += word;
+//             if (i < s.length() - 1)
+//                 res += "-";
+//         }
+//         return res;
+//     }
+// };
+
+// #2
 class Accumul
 {
   public:
     static std::string accum(const std::string &s)
     {
-        std::string res;
+        std::string result;
         for (int i = 0; i < s.length(); i++)
         {
-            std::string word;
-            for (int j = 0; j <= i; j++)
-                if (j == 0)
-                    word += std::toupper(s[i]);
-                else
-                    word += std::tolower(s[i]);
-            res += word;
-            if (i < s.length() - 1)
-                res += "-";
+            result.append("-");
+            result.append(std::string(1, toupper(s[i])));
+            result.append(std::string(i, tolower(s[i])));
         }
-        return res;
+        return result.substr(1, result.length());
     }
 };
