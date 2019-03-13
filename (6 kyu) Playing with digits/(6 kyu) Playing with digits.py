03d58859f7b1eae4e0a136a5bc4796d42decfcc1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-03-13 14:07:42 +0100

    refactoring

--- (6 kyu) Playing with digits/(6 kyu) Playing with digits.py
+++ (6 kyu) Playing with digits/(6 kyu) Playing with digits.py
@@ -1,7 +1,19 @@
-def dig_pow(n, p):
+def dig_pow(n, p):  # #1
     string = str(n)
     res = 0
     for s in string:
         res += int(s) ** p
         p += 1
     return res / n if res % n == 0 else -1
+
+
+def dig_pow(n, p):  # #2
+    res = 0
+    for i, s in enumerate(str(n)):
+        res += int(s) ** (p + i)
+    return res / n if res % n == 0 else -1
+
+
+def dig_pow(n, p):  # #3
+    res = sum(int(s) ** (p + i) for i, s in enumerate(str(n)))
+    return res // n if res % n == 0 else -1
