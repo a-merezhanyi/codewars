7b753fca3dc502242f4dba82135dbd2c920d3df1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-04-23 18:36:56 +0200

    filed TS types

--- (6 kyu) Square Digits Sequence/(6 kyu) Square Digits Sequence.ts
+++ (6 kyu) Square Digits Sequence/(6 kyu) Square Digits Sequence.ts
@@ -1,10 +1,10 @@
 export function squareDigitsSequence(a: number): number {
-  const nums = [];
+  const nums: number[] = [];
   while (-1 === nums.indexOf(a)) {
     nums.push(a);
     a = String(a)
       .split('')
-      .reduce((s, n) => s + Math.pow(+n, 2), 0);
+      .reduce((s: number, n: string) => s + Math.pow(Number(n), 2), 0);
   }
   return nums.length + 1;
 }
