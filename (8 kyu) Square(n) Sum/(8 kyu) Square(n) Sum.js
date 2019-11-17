ad94e5401a0dd0bc34c220d85790d81e34ac69c0
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-11-17 19:45:49 +0100

    Update (8 kyu) Square(n) Sum.js

--- (8 kyu) Square(n) Sum/(8 kyu) Square(n) Sum.js
+++ (8 kyu) Square(n) Sum/(8 kyu) Square(n) Sum.js
@@ -1,6 +1,24 @@
 // #1
+// function squareSum(numbers)
+// { 
+//   let totalSum = 0;
+//   for (i=0; i < numbers.length; i++)
+//   {
+//     totalSum += Math.pow(numbers[i], 2);
+//   }
+//   return totalSum;
+// }
+// #2
+// function squareSum(numbers){
+//   let totalSum = 0;
+//   numbers.forEach(function(x) {
+//     totalSum +=  Math.pow(x, 2)
+//   });
+//   return totalSum;
+// }
+// #3
 // function squareSum(numbers){
 //     return numbers.reduce((a, x) => a + (x * x), 0)
 // }
-// #2
+// #4
 const squareSum = (numbers) => numbers.reduce((a, x) => a + x * x, 0);
