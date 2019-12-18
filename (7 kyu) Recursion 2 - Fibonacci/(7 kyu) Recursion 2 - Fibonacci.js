92f8ee306c32fa41823ee3dc180f1b46af9fa593
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-12-18 07:49:08 +0100

    Update (7 kyu) Recursion 2 - Fibonacci.js

--- (7 kyu) Recursion 2 - Fibonacci/(7 kyu) Recursion 2 - Fibonacci.js
+++ (7 kyu) Recursion 2 - Fibonacci/(7 kyu) Recursion 2 - Fibonacci.js
@@ -1,7 +1,29 @@
-const fibonacci = (n) => {
-  if (n < 3) {
-    return 1;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
-};
+// #1
+// const fibonacci = n => {
+//   let sum = 0
+//   if (n === 1 || n === 2) {
+//     sum = 1
+//   }
+//   else {
+//     sum += fibonacci(n-1) + fibonacci(n-2)
+//   }
+//   return sum
+// }
+// #2
+// const fibonacci = (n) => {
+//   if (n < 3) {
+//     return 1;
+//   } else {
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+// }
+// #3
+// const fibonacci = n => {
+//   return (
+//     n < 3
+//       ? 1
+//       : fibonacci(n-1) + fibonacci(n-2)
+//     )
+// }
+// #4
+const fibonacci = n => n < 3 ? 1 : fibonacci(n-1) + fibonacci(n-2)
