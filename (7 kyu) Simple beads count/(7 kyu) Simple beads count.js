d4549e1c7a7f64aff2e21ed24874e8da293819fc
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-12-18 07:33:59 +0100

    Update (7 kyu) Simple beads count.js

--- (7 kyu) Simple beads count/(7 kyu) Simple beads count.js
+++ (7 kyu) Simple beads count/(7 kyu) Simple beads count.js
@@ -1,11 +1,19 @@
 // #1
 // function countRedBeads(n) {
-//   return n < 2 ? 0 : (n - 1) * 2;
+//   if (n < 2) {
+//     return 0;
+//   } else {
+//     return (n - 1) * 2;
+//   }
 // }
 // #2
-// const countRedBeads = (n) => (n < 2 ? 0 : (n - 1) * 2);
+// function countRedBeads(n) {
+//   return n < 2 ? 0 : (n - 1) * 2;
+// }
 // #3
-// const countRedBeads = (n) => ((n || 1) - 1) * 2;
+// const countRedBeads = (n) => (n < 2 ? 0 : (n - 1) * 2);
 // #4
+// const countRedBeads = (n) => ((n || 1) - 1) * 2;
+// #5
 // maximum value -> two for every two beads times number of blue beads - 1
 const countRedBeads = (n) => Math.max(0, (n - 1) * 2);
