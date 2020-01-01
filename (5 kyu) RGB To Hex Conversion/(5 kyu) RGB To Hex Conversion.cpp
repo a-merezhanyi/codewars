9ce749066faff937c8814b228677c371699bc1dc
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-01-01 20:02:51 +0100

    Update (5 kyu) RGB To Hex Conversion.cpp

--- (5 kyu) RGB To Hex Conversion/(5 kyu) RGB To Hex Conversion.cpp
+++ (5 kyu) RGB To Hex Conversion/(5 kyu) RGB To Hex Conversion.cpp
@@ -3,19 +3,18 @@
 
 class RGBToHex
 {
-  static inline std::string decToHex(int c)
+  static inline std::string dec_to_hex(int c)
   {
     if (c < 0) return "00";
     else if (c > 255) return "FF";
     std::ostringstream oss;
-    oss << std::uppercase << std::setfill('0') << std::hex
-        << std::setw(2) << c;
+    oss << std::uppercase << std::setfill('0') << std::hex << std::setw(2) << c;
     return oss.str();
   }
   
   public:
   static std::string rgb(int r, int g, int b)
   {
-    return decToHex(r) + decToHex(g) + decToHex(b);
+    return dec_to_hex(r) + dec_to_hex(g) + dec_to_hex(b);
   }
 };
