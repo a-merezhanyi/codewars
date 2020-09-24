814ccf2dfd5acccc9102b99646cbc805b21db4ba
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-09-24 18:09:28 +0200

    Update (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.java

--- (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.java
+++ (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.java
@@ -1,17 +1,31 @@
-public class StringUtils {
-  
-  public static String toAlternativeString(String string) {
-    String result = "";
-    for (int i =0; i < string.length(); i++) {
-      String c = Character.toString(string.charAt(i));
-      if (c.toUpperCase() == c) {
-        result += c.toLowerCase();
-      } else {
-        result += c.toUpperCase();
-      }
-    }
+// #1
+// public class StringUtils {
+//   public static String toAlternativeString(String string) {
+//     String result = "";
+//     for (int i =0; i < string.length(); i++) {
+//       String c = Character.toString(string.charAt(i));
+//       if (c.toUpperCase() == c) {
+//         result += c.toLowerCase();
+//       } else {
+//         result += c.toUpperCase();
+//       }
+//     }
     
-    return result;
-  }
+//     return result;
+//   }
+// }
 
+// #2
+public class StringUtils {
+    public static String toAlternativeString(String string) {
+        String result = "";
+        for (char c : string.toCharArray()) {
+            if(Character.isUpperCase(c)) {
+                result += Character.toLowerCase(c);
+            } else {
+                result += Character.toUpperCase(c);
+            }
+        }
+        return result;
+    }
 }
