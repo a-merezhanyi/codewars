de4776f08d5fe38c3738298ced74d45f6703879d
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-10-18 20:37:12 +0200

    Update (8 kyu) The Feast of Many Beasts.java

--- (8 kyu) The Feast of Many Beasts/(8 kyu) The Feast of Many Beasts.java
+++ (8 kyu) The Feast of Many Beasts/(8 kyu) The Feast of Many Beasts.java
@@ -1,10 +1,23 @@
+// #1
+// public class Kata {
+//   public static boolean feast(String beast, String dish) {
+//     String b1 = beast.substring(0, 1);
+//     String d1 = dish.substring(0, 1);
+//     String b2 = beast.substring(beast.length() - 1, beast.length());
+//     String d2 = dish.substring(dish.length() - 1, dish.length());
+//    
+//     return b1.equals(d1) && b2.equals(d2);
+//   }
+// }
+
+// #2
 public class Kata {
   public static boolean feast(String beast, String dish) {
-    String b1 = beast.substring(0, 1);
-    String d1 = dish.substring(0, 1);
-    String b2 = beast.substring(beast.length() - 1, beast.length());
-    String d2 = dish.substring(dish.length() - 1, dish.length());
+    char b1 = beast.charAt(0);
+    char d1 = dish.charAt(0);
+    char b2 = beast.charAt(beast.length() - 1);
+    char d2 = dish.charAt(dish.length() - 1);
     
-    return b1.equals(d1) && b2.equals(d2);
+    return b1 == d1 && b2 == d2;
   }
 }
