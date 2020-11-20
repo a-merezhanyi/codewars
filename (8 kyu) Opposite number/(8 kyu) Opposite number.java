2d71d64c4a431336cc5f27b27ea6b9cec26400fb
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-11-20 23:37:06 +0100

    Create (8 kyu) Opposite number.java

---
+++ (8 kyu) Opposite number/(8 kyu) Opposite number.java
@@ -0,0 +1,13 @@
+// #1
+// public class Kata {
+//   public static int opposite(int number) {
+//     return number * -1;
+//   }
+// }
+
+// #2
+public class Kata {
+  public static int opposite(int number) {
+    return -number;
+  }
+}
