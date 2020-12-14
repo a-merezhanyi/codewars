930670187f14b6e30a6484eeb92d253b2197fb07
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-12-14 13:34:36 +0100

    Update (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.js

--- (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.js
+++ (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.js
@@ -1,5 +1,54 @@
+// #1 Plain solution
+String.prototype.toAlternatingCase = function() {
+  res = "";
+  for(let i = 0; i < this.length; i++) {
+    if (this[i] === this[i].toUpperCase()) {
+      res += this[i].toLowerCase();
+    } else {
+      res += this[i].toUpperCase();
+    }
+  }
+  return res;
+}
+
+// #2 Straightforward solution
+String.prototype.toAlternatingCase = function(res = "") {
+  for(let i = 0; i < this.length; i++) {
+    res += this[i] === this[i].toUpperCase()
+      ? this[i].toLowerCase()
+      : this[i].toUpperCase();
+  }
+  return res;
+}
+
+// #3 Optimized solution
 String.prototype.toAlternatingCase = function() {
   return [...this].map(
-    (c) => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()),
-  ).join``;
+    c => (c === c.toUpperCase()
+          ? c.toLowerCase()
+          : c.toUpperCase()),
+  ).join('');
 };
+
+// #4 Clever solution
+String.prototype.toAlternatingCase = function() {
+  return this.
+    replace(
+      /[A-Za-z]/g,
+      x => x > "Z"
+        ? x.toUpperCase()
+        : x.toLowerCase()
+    ) 
+}
+
+// #5 Coding golf
+String.prototype
+  .toAlternatingCase = function() {
+  return this.
+    replace(
+      /[A-z]/g,
+      x => x["to"
+        + (x > "Z" ? "Upper" : "Lower")
+        + "Case"]()
+    ) 
+}
