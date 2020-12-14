9c3b4f4311028a203bcda0e0cdbf455e27929f5f
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-12-14 13:41:20 +0100

    Update (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.rs

--- (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.rs
+++ (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.rs
@@ -1,29 +1,35 @@
+// #1
 fn to_alternating_case(s: &str) -> String {
-    // #1
-    // let mut result = String::new();
-    // for (i, c) in s.chars().enumerate() {
-    //     if c.is_lowercase() {
-    //         let x = c.to_string().to_uppercase().to_owned();
-    //         result.push_str(&x);
-    //     } else if c.is_uppercase() {
-    //         let x = c.to_string().to_lowercase().to_owned();
-    //         result.push_str(&x);
-    //     } else {
-    //         result.push(c);
-    //     }
-    // }
-    // return result;
-    // #2
-    // let mut result = String::new();
-    // for c in s.chars() {
-    //     if c.is_lowercase() {
-    //         result.extend(c.to_uppercase());
-    //     } else {
-    //         result.extend(c.to_lowercase());
-    //     }
-    // }
-    // result
-    // #3
+    let mut result = String::new();
+    for (i, c) in s.chars().enumerate() {
+        if c.is_lowercase() {
+            let x = c.to_string().to_uppercase().to_owned();
+            result.push_str(&x);
+        } else if c.is_uppercase() {
+            let x = c.to_string().to_lowercase().to_owned();
+            result.push_str(&x);
+        } else {
+            result.push(c);
+        }
+    }
+    return result;
+}
+
+// #2
+fn to_alternating_case(s: &str) -> String {
+    let mut result = String::new();
+    for c in s.chars() {
+        if c.is_lowercase() {
+            result.extend(c.to_uppercase());
+        } else {
+            result.extend(c.to_lowercase());
+        }
+    }
+    result
+}
+
+// #3
+fn to_alternating_case(s: &str) -> String {
     s.chars()
         .filter_map(|c| {
             if c.is_lowercase() {
