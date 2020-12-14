209df9e057e67d5481cf530cd99ea852e60d10e2
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-12-14 13:38:52 +0100

    Update (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.java

--- (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.java
+++ (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe/(8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.java
@@ -1,18 +1,18 @@
 // #1
-// public class StringUtils {
-//   public static String toAlternativeString(String string) {
-//     String result = "";
-//     for (int i =0; i < string.length(); i++) {
-//       String c = Character.toString(string.charAt(i));
-//       if (c.toUpperCase() == c) {
-//         result += c.toLowerCase();
-//       } else {
-//         result += c.toUpperCase();
-//       }
-//     }
-//     return result;
-//   }
-// }
+public class StringUtils {
+  public static String toAlternativeString(String string) {
+    String result = "";
+    for (int i =0; i < string.length(); i++) {
+      String c = Character.toString(string.charAt(i));
+      if (c.toUpperCase() == c) {
+        result += c.toLowerCase();
+      } else {
+        result += c.toUpperCase();
+      }
+    }
+    return result;
+  }
+}
 
 // #2
 public class StringUtils {
