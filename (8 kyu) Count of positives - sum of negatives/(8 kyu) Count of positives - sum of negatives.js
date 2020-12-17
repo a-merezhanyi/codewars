efd1275d7c42ed859835c646c082524b5e73867c
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-12-17 19:37:02 +0100

    Update (8 kyu) Count of positives - sum of negatives.js

--- (8 kyu) Count of positives - sum of negatives/(8 kyu) Count of positives - sum of negatives.js
+++ (8 kyu) Count of positives - sum of negatives/(8 kyu) Count of positives - sum of negatives.js
@@ -1,20 +1,25 @@
+// 1 Plain Solution
 function countPositivesSumNegatives(input) {
-  // #1
-  //   if (!input || !input.length) {
-  //     return [];
-  //   }
-  //   const res = [0, 0];
-  //   input.forEach((x) => (x > 0 ? res[0]++ : (res[1] += x)));
-  //   return res;
-  // #2
-  //   return input && input.length
-  //     ? [
-  //         input.filter((p) => p > 0).length,
-  //         input.filter((n) => n < 0).reduce((a, b) => a + b, 0),
-  //       ]
-  //     : [];
-  // #3
+  if (!input || !input.length) {
+    return [];
+  }
+  const res = [0, 0];
+  input.forEach((x) => (x > 0 ? res[0]++ : (res[1] += x)));
+
+  return res;
+}
+// 2 Optimized solution
+function countPositivesSumNegatives(input) {
   return input && input.length
+    ? [
+        input.filter((p) => p > 0).length,
+        input.filter((n) => n < 0).reduce((a, b) => a + b, 0),
+      ]
+    : [];
+}
+// 3 Coding golf
+const countPositivesSumNegatives = input =>
+  input && input.length
     ? input.reduce(
         (p, x) => {
           x > 0 ? p[0]++ : (p[1] += x);
@@ -23,4 +28,3 @@ function countPositivesSumNegatives(input) {
         [0, 0],
       )
     : [];
-}
