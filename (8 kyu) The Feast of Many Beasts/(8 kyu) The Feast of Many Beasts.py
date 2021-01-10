83eed3e21d81edb8f9b71dd0165a8fd8196d7635
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-01-10 23:25:05 +0100

    Update (8 kyu) The Feast of Many Beasts.py

--- (8 kyu) The Feast of Many Beasts/(8 kyu) The Feast of Many Beasts.py
+++ (8 kyu) The Feast of Many Beasts/(8 kyu) The Feast of Many Beasts.py
@@ -1,9 +1,14 @@
+# 1 Plain Solution
 def feast(beast, dish):
-    # #1
-    #    start = beast.startswith(dish[:1])
-    #    end = beast.endswith(dish[-1:])
-    #    return start and end
-    # #2
-    #     return beast.startswith(dish[:1]) and beast.endswith(dish[-1:])
-    # #3
+   start = beast.startswith(dish[:1])
+   end = beast.endswith(dish[-1:])
+    
+   return start and end
+
+# 2 Optimized solution
+def feast(beast, dish):
+    return beast.startswith(dish[:1]) and beast.endswith(dish[-1:])
+        
+# 3 Clever solution
+def feast(beast, dish):
     return beast[0] == dish[0] and beast[-1] == dish[-1]
