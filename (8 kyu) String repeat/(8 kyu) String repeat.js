5705f2e0e473aaf5f5edf06405b7acd493d64a8d
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-01-11 18:12:30 +0100

    Update (8 kyu) String repeat.js

--- (8 kyu) String repeat/(8 kyu) String repeat.js
+++ (8 kyu) String repeat/(8 kyu) String repeat.js
@@ -1,14 +1,25 @@
-// #1
-// function repeatStr (n, s) {
-//     let str = '';
-//     for (let i = 0; i < n; i++) {
-//         str += s;
-//     }
-//     return str;
-// }
-// #2
-// function repeatStr (n, s) {
-//     return s.repeat(n)
-// }
-// #3
-const repeatStr = (n, s) => s.repeat(n);
+// 1 Plain Solution
+function repeatStr (n, s) {
+  let str = '';
+  
+  for (let i = 0; i < n; i++) {
+    str = str + s;
+  }
+  
+  return str;
+}
+
+// 2 Optimized solution
+function repeatStr (n, s) {
+  return (
+    new Array(n).fill(s)
+  ).join('');
+}
+
+// 3 Clever solution
+function repeatStr (n, s) {
+    return s.repeat(n)
+}
+
+// 4 Coding golf
+repeatStr = (n, s) => s.repeat(n);
