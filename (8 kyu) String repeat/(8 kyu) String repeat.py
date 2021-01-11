168aaaedf8ef6d35af20c07b0d917b808f904148
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-01-11 18:18:00 +0100

    Update (8 kyu) String repeat.py

--- (8 kyu) String repeat/(8 kyu) String repeat.py
+++ (8 kyu) String repeat/(8 kyu) String repeat.py
@@ -1,8 +1,19 @@
+# 1 Plain Solution
 def repeat_str(repeat, string):
-    # #1
-    # str = ""
-    # for i in range(repeat):
-    #     str += string
-    # return str
-    #2
+    str = ""
+    
+    for i in range(repeat):
+        str = str + string
+        
+    return str
+
+# 2 Optimized solution
+def repeat_str(repeat, string):
+    return "{}".format(string) * int(repeat)
+
+# 3 Clever solution
+def repeat_str(repeat, string):
     return repeat * string
+
+# 4 Coding golf
+repeat_str = lambda n, str: str * n
