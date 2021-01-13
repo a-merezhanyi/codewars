b5ba99ad2d65544dda21e6b973e971bcc77f3c28
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-01-13 22:04:19 +0100

    Update (8 kyu) Square(n) Sum.js

--- (8 kyu) Square(n) Sum/(8 kyu) Square(n) Sum.js
+++ (8 kyu) Square(n) Sum/(8 kyu) Square(n) Sum.js
@@ -1,24 +1,36 @@
-// #1
-// function squareSum(numbers)
-// { 
-//   let totalSum = 0;
-//   for (i=0; i < numbers.length; i++)
-//   {
-//     totalSum += Math.pow(numbers[i], 2);
-//   }
-//   return totalSum;
-// }
-// #2
-// function squareSum(numbers){
-//   let totalSum = 0;
-//   numbers.forEach(function(x) {
-//     totalSum +=  Math.pow(x, 2)
-//   });
-//   return totalSum;
-// }
-// #3
-// function squareSum(numbers){
-//     return numbers.reduce((a, x) => a + (x * x), 0)
-// }
-// #4
-const squareSum = (numbers) => numbers.reduce((a, x) => a + x * x, 0);
+// 1 Plain solution
+function squareSum(numbers) { 
+  let totalSum = 0;
+  
+  for (i=0; i < numbers.length; i++) {
+    totalSum += Math.pow(numbers[i], 2);
+  }
+  
+  return totalSum;
+}
+
+// 2 Optimized solution
+function squareSum(numbers){
+  let totalSum = 0;
+  
+  numbers.forEach(x =>
+    totalSum +=  Math.pow(x, 2)
+  );
+  
+  return totalSum;
+}
+
+// 3 Clever solution
+function squareSum(numbers){
+    return numbers
+      .reduce(
+        (acc, x) => acc + (x * x)
+        , 0)
+}
+
+// 4 Coding golf
+squareSum = n => n
+  .reduce(
+    (a, x) => a + x * x
+    , 0
+  );
