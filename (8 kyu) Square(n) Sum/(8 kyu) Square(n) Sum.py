9254fb75d2291c30d5ec143b58dbe7f5c52fe900
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-01-13 22:01:57 +0100

    Update (8 kyu) Square(n) Sum.py

--- (8 kyu) Square(n) Sum/(8 kyu) Square(n) Sum.py
+++ (8 kyu) Square(n) Sum/(8 kyu) Square(n) Sum.py
@@ -1,8 +1,22 @@
+# 1 Plain solution
 def square_sum(numbers):
-    # #1
-    # sum = 0
-    # for x in numbers:
-    #     sum += x * x
-    # return sum
-    # #2
+    sum = 0
+    
+    for x in numbers:
+        sum += x * x
+        
+    return sum
+
+# 2 Optimized solution
+def square_sum(numbers):
+    res = []
+    for x in numbers:
+        res.append(x**2)
+    return(sum(res))
+
+# 3 Clever solution
+def square_sum(numbers):
     return sum(x ** 2 for x in numbers)
+
+# 4 Coding golf
+square_sum = lambda n: sum(x**2 for x in n)
