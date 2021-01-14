90a422503d1f8f87e91d128f42d806555a05582b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-01-14 17:56:32 +0100

    Update (8 kyu) Sort and Star.js

--- (8 kyu) Sort and Star/(8 kyu) Sort and Star.js
+++ (8 kyu) Sort and Star/(8 kyu) Sort and Star.js
@@ -1,16 +1,37 @@
-// #1
-// function twoSort(s) {
-//   return s
-//     .sort((a, b) => a < b ? -1 : 1)[0]
-//     .split("")
-//     .join("***");
-// }
-// #2
-// function twoSort(s) {
-//   return s
-//     .sort()[0]
-//     .split("")
-//     .join("***");
-// }
-// #3
-const twoSort = (s) => [...s.sort()[0]].join('***');
+// 1 Plain solution
+function twoSort(s) {
+  const last = s.sort(
+    (a, b) => a < b ? -1 : 1
+  )[0];
+  let res = last[0];
+  
+  for(let i = 1; i < last.length; i++) {
+    res += "***" + last[i];
+  }
+  
+  return res;
+}
+
+// 2 Optimized solution
+function twoSort(s) {
+  const res = s.sort(
+    (a, b) => a < b ? -1 : 1
+  )[0];
+  
+  return res
+    .split("")
+    .join("***");
+}
+
+// 3 Clever solution
+function twoSort(s) {
+  return s
+    .sort()[0]
+    .split``
+    .join`***`;
+}
+
+// 4 Coding golf
+twoSort = s => [
+  ...s.sort()[0]
+].join('***')
