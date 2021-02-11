181a9a2610dba0a24e14c83fc5bf04521ecb4f61
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-02-11 21:52:30 +0100

    Update (8 kyu) Reversed sequence.js

--- (8 kyu) Reversed sequence/(8 kyu) Reversed sequence.js
+++ (8 kyu) Reversed sequence/(8 kyu) Reversed sequence.js
@@ -1,4 +1,23 @@
+// 1 Plain solution
+const reverseSeq = n => {
+  let res = [];
+
+  for (let i = n; i > 0; i--) {
+    res.push(i);
+  }
+  
+  return res;
+};
+
+// 2 Clever solution
 const reverseSeq = (n) =>
   Array(n)
     .fill()
     .map((x, i) => n - i);
+
+// 3 Coding golf
+const reverseSeq = length =>
+  Array.from(
+    {length},
+    () => length--
+  )
