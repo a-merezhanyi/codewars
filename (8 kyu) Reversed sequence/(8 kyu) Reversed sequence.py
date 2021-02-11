1bd00d845ad839f50036c2760486cfe9c1e03637
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-02-11 21:57:57 +0100

    Update (8 kyu) Reversed sequence.py

--- (8 kyu) Reversed sequence/(8 kyu) Reversed sequence.py
+++ (8 kyu) Reversed sequence/(8 kyu) Reversed sequence.py
@@ -1,8 +1,17 @@
+# 1 Plain solution
 def reverse_seq(n):
-    # #1
-    #     arr = []
-    #     for i in range(n):
-    #         arr.append(n - i)
-    #     return arr
-    # #2
+    arr = []
+    for i in range(n):
+        arr.append(n - i)
+    return arr
+
+# 2 Optimized solution
+def reverse_seq(n):
     return [x for x in range(n, 0, -1)]
+
+# 3 Clever solution
+def reverse_seq(n):
+    return list(range(n, 0, -1))
+
+# 4 Coding golf
+reverse_seq = lambda n: list(range(n, 0, -1))
