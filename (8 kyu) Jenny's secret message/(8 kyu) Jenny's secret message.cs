916f301c5e049542c88871cdbfe57fbdd0a439c5
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-04-25 13:09:37 +0200

    Create  (8 kyu) Jenny's secret message.cs

---
+++ (8 kyu) Jenny's secret message/(8 kyu) Jenny's secret message.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Kata
+{
+  public static string greet(string name)
+  {
+    if(name == "Johnny")
+      return "Hello, my love!";
+    return "Hello, " + name + "!";
+  }
+}
