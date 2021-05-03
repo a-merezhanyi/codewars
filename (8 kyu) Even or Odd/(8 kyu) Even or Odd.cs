7a59b411e3ce77bd17dd1d6bc7635ca2a5d02202
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-05-03 20:47:58 +0200

    Create (8 kyu) Even or Odd.cs

---
+++ (8 kyu) Even or Odd/(8 kyu) Even or Odd.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Solution
+{
+  public class SolutionClass
+  {
+    public static string EvenOrOdd(int number)
+    {
+      return number % 2 == 0 ? "Even":"Odd";
+    }
+  }
+}
