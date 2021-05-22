e4e5dc79e4edfc2651b2ba6c92f5640715b2e39a
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-05-22 23:29:04 +0200

    Update (7 kyu) Two to One.cs

--- (7 kyu) Two to One/(7 kyu) Two to One.cs
+++ (7 kyu) Two to One/(7 kyu) Two to One.cs
@@ -1,3 +1,4 @@
+// #1
 using System.Collections.Generic;
 using System.Linq;
 
@@ -20,5 +21,16 @@ public class TwoToOne
     var res = arr.OrderBy(x=>x).ToArray();
     
     return string.Join("", res);
+  }
+}
+
+// #2
+using System.Linq;
+
+public class TwoToOne
+{
+  public static string Longest (string s1, string s2) 
+  {
+    return new string((s1 + s2).Distinct().OrderBy(x=>x).ToArray());
   }
 }
