31ba7a157fcd19d784b68078a879e5f8bcde02a0
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-08-13 00:17:27 +0200

    Create (7 kyu) Number of People in the Bus.java

---
+++ (7 kyu) Number of People in the Bus/(7 kyu) Number of People in the Bus.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Metro {
+
+  public int countPassengers(ArrayList<int[]> stops) {
+    int totalPeople = 0;
+    
+    for(int[] stop : stops) {
+      totalPeople += stop[0] - stop[1];
+    }
+    
+    return people;
+  }
+}
