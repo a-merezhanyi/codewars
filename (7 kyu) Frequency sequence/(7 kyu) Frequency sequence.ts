544024f6621dcd2528f2f156ed3d0f817cc0a9a1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2021-10-11 17:29:49 +0200

    Update (7 kyu) Frequency sequence.ts

--- (7 kyu) Frequency sequence/(7 kyu) Frequency sequence.ts
+++ (7 kyu) Frequency sequence/(7 kyu) Frequency sequence.ts
@@ -1,14 +1,19 @@
 export function freqSeq(str: string, sep: string): string {
-  const freq = {};
+  type FreqType = {
+    [key: string]: number
+  }
+  const freq: FreqType = {};
   let newStr = '';
-
+  
   for (let i = 0; i < str.length; i++) {
-    freq[str[i]] = freq[str[i]] ? (freq[str[i]] += 1) : 1;
+    const n = String(str[i]);
+    freq[n] = freq[n] ? freq[n] += 1 : 1;
   }
-
+  
   for (let i = 0; i < str.length; i++) {
-    newStr += i ? sep + freq[str[i]] : freq[str[i]];
+    const n = String(str[i]);
+    newStr += i ? sep + String(freq[n]) : String(freq[n]);
   }
-
+  
   return newStr;
 }
