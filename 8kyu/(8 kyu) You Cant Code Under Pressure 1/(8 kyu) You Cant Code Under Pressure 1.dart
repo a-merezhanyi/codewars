84a7511efd593f492f5ba4301926428d4e7ca456
A. Merezhanyi <a.merezhanyi@gmail.com>
2023-05-03 21:37:37 +0200

    Update (8 kyu) You Cant Code Under Pressure 1.dart

--- 8kyu/(8 kyu) You Cant Code Under Pressure 1/(8 kyu) You Cant Code Under Pressure 1.dart
+++ 8kyu/(8 kyu) You Cant Code Under Pressure 1/(8 kyu) You Cant Code Under Pressure 1.dart
@@ -1,17 +1,1 @@
-// 1 Plain solution
-int doubleInteger(int i) {
-  return i * 2;
-}
-
-// 2 Optimized solution
-int doubleInteger(int i) {
-  return i + i;
-}
-
-// 3 Clever solution
-int doubleInteger(int i) {
-  return i << 1;
-}
-
-// 4 Coding golf
 int doubleInteger(int i) => i << 1;
