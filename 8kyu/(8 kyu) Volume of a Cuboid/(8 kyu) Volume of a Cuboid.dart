8d953c7772480e307bcd09b58f4f83fd4c0bbd2e
A. Merezhanyi <a.merezhanyi@gmail.com>
2024-05-14 08:35:22 +0200

    solved "8kyu Volume of a Cuboid" in Dart

---
+++ 8kyu/(8 kyu) Volume of a Cuboid/(8 kyu) Volume of a Cuboid.dart
@@ -0,0 +1,12 @@
+// 1 Plain solution
+double getVolumeOfCuboid(final double length, final double width, final double height) {
+  return length * width * height;
+}
+
+// 2 Optimized solution
+double getVolumeOfCuboid(final double l, final double w, final double h) {
+  return l * w * h;
+}
+
+// 3 Clever solution
+double getVolumeOfCuboid(final l, final w, final h) => l * w * h;
