5930e805fdf4ecc7c17a474b007c5907bf77c8bb
A. Merezhanyi <a.merezhanyi@gmail.com>
2025-02-26 09:57:20 +0100

    Added TS variants for You Can't Code Under Pressure #1

--- 8kyu/(8 kyu) You Cant Code Under Pressure 1/(8 kyu) You Cant Code Under Pressure 1.ts
+++ 8kyu/(8 kyu) You Cant Code Under Pressure 1/(8 kyu) You Cant Code Under Pressure 1.ts
@@ -1,3 +1,10 @@
+// 1 Plain solution
 export function doubleInteger(i: number): number {
-    return 2 * i;
+  return 2 * i;
 }
+
+// 2 Optimized solution
+export const doubleInteger = (i: number): number => 2 * i;
+
+// 3 Coding golf
+export const doubleInteger = (i: any) => i << 1;
