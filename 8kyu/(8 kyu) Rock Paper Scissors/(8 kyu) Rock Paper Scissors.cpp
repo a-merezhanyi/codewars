ae296d14fba19e77d781dbaf640069a7f1bb2b7b
A. Merezhanyi <a.merezhanyi@gmail.com>
2025-08-04 20:11:48 +0200

    "(8 kyu) Rock Paper Scissors" solved in C++

---
+++ 8kyu/(8 kyu) Rock Paper Scissors/(8 kyu) Rock Paper Scissors.cpp
@@ -0,0 +1,40 @@
+include <string>
+
+// 1 Plain solution
+std::string rps(const std::string& p1, const std::string& p2)
+{
+    if (p1 == "scissors" && p2 == "paper") {
+        return "Player 1 won!";
+    } else if (p1 == "scissors" && p2 == "rock") {
+        return "Player 2 won!";
+    } else if (p1 == "rock" && p2 == "paper") {
+        return "Player 2 won!";
+    } else if (p1 == "rock" && p2 == "scissors") {
+        return "Player 1 won!";
+    } else if (p1 == "paper" && p2 == "rock") {
+        return "Player 1 won!";
+    } else if (p1 == "paper" && p2 == "scissors") {
+        return "Player 2 won!";
+    } else {
+        return "Draw!";
+    }
+}
+
+// 2 Optimized solution
+std::string rps(const std::string& p1, const std::string& p2)
+{
+    if (p1 == p2) return "Draw!";
+    if (p1 == "rock" && p2 == "scissors") return "Player 1 won!";
+    if (p1 == "scissors" && p2 == "paper") return "Player 1 won!";
+    if (p1 == "paper" && p2 == "rock") return "Player 1 won!";
+    return "Player 2 won!";
+}
+
+// 3 Clever solution
+std::string rps(const std::string& p1, const std::string& p2)
+{
+    if (p1 == p2) return "Draw!";
+    return "Player " + std::to_string((p1 == "rock" && p2 == "scissors") ||
+                                      (p1 == "scissors" && p2 == "paper") ||
+                                      (p1 == "paper" && p2 == "rock") ? 1 : 2) + " won!";
+}
